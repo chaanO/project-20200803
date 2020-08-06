d34d1b8f7697c90a491b67d9c07c926d4f4f8e8a
chanoh-park <cocosh6@naver.com>
2020-08-06 14:16:43 +0900

    cart

--- src/cart/model/Cart.java
+++ src/cart/model/Cart.java
@@ -1,46 +1,26 @@
 package cart.model;
 
-import java.util.Date;
-
 public class Cart {
-	private int bNum;
-	private String bName;
-	private int price;
+	private int cartId;
+	private int bookId;
 	private int amount;
-	private Date regDate;
 	
-	public int getbNum() {
-		return bNum;
+	public int getCartId() {
+		return cartId;
 	}
-	public void setbNum(int bNum) {
-		this.bNum = bNum;
+	public void setCartId(int cartId) {
+		this.cartId = cartId;
 	}
-	
-	public String getbName() {
-		return bName;
+	public int getBookId() {
+		return bookId;
 	}
-	public void setbName(String bName) {
-		this.bName = bName;
+	public void setBookId(int bookId) {
+		this.bookId = bookId;
 	}
-	
-	public int getPrice() {
-		return price;
-	}
-	public void setPrice(int price) {
-		this.price = price;
-	}
-	
 	public int getAmount() {
 		return amount;
 	}
 	public void setAmount(int amount) {
 		this.amount = amount;
-	}
-	
-	public Date getRegDate() {
-		return regDate;
-	}
-	public void setRegDate(Date regDate) {
-		this.regDate = regDate;
 	}
 }
---
+++ src/cart/model/Product.java
@@ -0,0 +1,26 @@
+package cart.model;
+
+public class Product {
+	private int bookId;
+	private String bookName;
+	private int price;
+	
+	public int getBookId() {
+		return bookId;
+	}
+	public void setBookId(int bookId) {
+		this.bookId = bookId;
+	}
+	public String getBookName() {
+		return bookName;
+	}
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+}
